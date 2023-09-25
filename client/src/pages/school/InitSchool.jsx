afcb170271eb70b65b3433cf36344c708c521b61
mzacom <oyetolamuiz78@gmail.com>
2023-09-25 10:07:15 -0700

    academy chnges

--- client/src/pages/school/InitSchool.jsx
+++ client/src/pages/school/InitSchool.jsx
@@ -10,7 +10,9 @@ const InitSchool = () => {
         <Link className=" flex items-center " to={"/"}>
           <i class="fa-solid  fa-chevron-left"></i> Back{" "}
         </Link>
-        <div className=" flex justify-center items-center text-center ">Academy</div>
+        <div className=" flex justify-center items-center text-center ">
+          Academy
+        </div>
         <i class="fa-solid fa-user"></i>
       </div>
       <section
@@ -18,10 +20,10 @@ const InitSchool = () => {
         id="home"
       >
         {/* school */}
-        <section className="  w-[88%] h-[130px] md:flex md:flex-col md:justify-center leading-tight md:h-[120px] mx-auto rounded-lg  bg-blue-600  shadow-lg">
+        <section className=" px-2 w-[88%] flex justify-center items-center h-[145px] md:flex md:flex-col md:justify-center leading-tight md:h-[120px] mx-auto rounded-lg  bg-blue-600  shadow-lg">
           <h1 className=" text-white md:flex md:flex-col  pt-2 md:text-3xl pl-4 text-xl ">
             School
-            <p className=" text-[small] px-1    ">
+            <p className=" text-[small]  ">
               Welcome to our Forex School, your gateway to mastering currency
               trading and financial independence.
             </p>
@@ -30,16 +32,16 @@ const InitSchool = () => {
 
         {/* Brokers and timetable */}
         <section className=" mx-auto w-[88%] gap-5  flex flex-col md:flex-row">
-          <div className="md:w-[40%] h-[100px] md:h-[120px] bg-slate-400 rounded-lg shadow-lg ">
+          <div className="md:w-[40%] h-[140px]  px-1 flex justify-center items-center md:h-[120px] bg-slate-400 rounded-lg shadow-lg ">
             <h1 className=" text-white pt-2 pl-4 text-xl md:text-left text-[15px] md:text-3xl ">
               Time-table
-              <p className=" text-[small] px-1   ">
+              <p className=" text-[small] px-2   ">
                 Explore our School timetable, a structured guide to your
                 educational journey.
               </p>
             </h1>
           </div>
-          <div className=" md:w-[60%] h-[130px] bg-pink-400  md:h-[120px] rounded-lg shadow-lg ">
+          <div className=" md:w-[60%]  px-1 flex justify-center items-center h-[160px] bg-pink-400  md:h-[120px] rounded-lg shadow-lg ">
             <h1 className=" text-white pt-2 pl-4 text-[15px] text-xl md:text-3xl ">
               Brokers.
               <p className=" text-[small] px-1  ">
@@ -52,7 +54,7 @@ const InitSchool = () => {
 
         {/* Market Time and Quiz */}
         <section className=" mx-auto w-[88%] gap-5  flex flex-col md:flex-row">
-          <div className="md:w-[60%] h-[110px] md:h-[120px] md:justify-center py-1 md:flex md:flex-col bg-slate-700 rounded-lg shadow-lg ">
+          <div className="md:w-[60%] h-[140px] md:h-[120px] md:justify-center flex justify-center items-center md:flex md:flex-col bg-slate-700 rounded-lg shadow-lg ">
             <h1 className=" text-white pt-2 pl-4 text-[15px] text-xl md:text-3xl ">
               Market Time
               <p className=" text-[small] px-1   ">
@@ -62,7 +64,7 @@ const InitSchool = () => {
             </h1>
           </div>
 
-          <div className="md:w-[40%] h-[135px] md:h-[120px] py-1  bg-orange-400 rounded-lg shadow-lg ">
+          <div className="md:w-[40%] h-[155px] md:h-[120px] flex justify-center items-center  bg-orange-400 rounded-lg shadow-lg ">
             <h1 className=" text-white pt-2 pl-4 text-[15px] text-xl md:text-3xl ">
               Quiz
               <p className=" text-[small] px-1   ">
@@ -74,12 +76,14 @@ const InitSchool = () => {
         </section>
 
         {/* Videos */}
-        <section className=" w-[88%] h-[190px] py-1 mx-auto rounded-lg md:h-[120px] bg-slate-400  shadow-lg">
+        <section className=" w-[88%] flex justify-center items-center h-[220px] px-1 mx-auto rounded-lg md:h-[120px] bg-slate-400  shadow-lg">
           <h1 className=" text-white pt-2 pl-4 text-xl md:text-3xl ">
             Videos
             <p className=" text-[small] px-1   ">
-              Step into our video realm , a captivating space wher knowledge comes to life. 
-              Immerse uyourself in informative and engaging contetnt that will broaden your understanding and ignite your curiosoty.
+              Step into our video realm , a captivating space wher knowledge
+              comes to life. Immerse uyourself in informative and engaging
+              contetnt that will broaden your understanding and ignite your
+              curiosoty.
             </p>
           </h1>
         </section>
