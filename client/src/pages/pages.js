b3f3cfd06252d4f061487f9322166983aa61706a
mzacom <oyetolamuiz78@gmail.com>
2024-02-29 07:28:44 -0800

    test

--- client/src/pages/pages.js
+++ client/src/pages/pages.js
@@ -1,7 +1,7 @@
 import LandingPage from "./landingPage/LandingPage";
 import SignIn from "./login/SignIn";
 import SignUp from "./signUp/SignUp";
-import Window from "./entrance/window";
+
 import DashBoard from "./dashBorad/DashBoard";
 import Notification from "./notification/Notification";
 import Academcy from "./academcy/Academcy";
@@ -12,6 +12,6 @@ export {
   SignUp,
   DashBoard,
   Notification,
-  Window,
+ 
   Academcy,
 };
