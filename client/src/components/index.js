82e9e133e67a4dfe7e6a8f79f8ae901a76033288
mzacom <oyetolamuiz78@gmail.com>
2024-02-29 08:25:57 -0800

    bugs won kill me

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,8 +3,15 @@ import Hero from "./Hero";
 import School from "./School";
 import CurrentMarket from "./CurrentMarket";
 import CallToAction from "./CallToAction";
-import WindowNav from "./windowNav";
+import WindowNav from "./WindowNav";
 import PhoneNav from "./PhoneNav";
 
-
-export { Navbar, Hero, School ,CurrentMarket,CallToAction, WindowNav,PhoneNav};
+export {
+  Navbar,
+  Hero,
+  School,
+  CurrentMarket,
+  CallToAction,
+  WindowNav,
+  PhoneNav,
+};
